6eb908244dd321cd94d41944e6e0bb72868c93aa
stanley-world <146215090+stanley-world@users.noreply.github.com>
2023-10-18 03:48:52 +0000

    index

--- dice.rb
+++ dice.rb
@@ -4,9 +4,7 @@ require "binding_of_caller"
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
-get("/") do
-  "Hello World"
-end
+
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
@@ -57,3 +55,16 @@ get("/dice/5/4") do
   "<h1>5dice4sides</h1>
    <p>#{outcome}</p>"
 end 
+
+get("/") do
+  "
+  <h1>Dice Roll</h1>
+
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    </ul>
+  "
+end
